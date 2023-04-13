d5493f29ac44f15f01a8ccf61da92488a023579d
dilane3 <komboudilane125@gmail.com>
2023-04-13 16:01:27 +0100

    Update style

--- src/App.jsx
+++ src/App.jsx
@@ -27,7 +27,6 @@ function App() {
               <svg
                 width="24"
                 height="24"
-                fill="none"
                 xmlns="http://www.w3.org/2000/svg"
               >
                 <path
